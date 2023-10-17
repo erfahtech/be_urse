7c2d063c21d0153703c1575a14b390f03fbb811a
erditona <erditonaushaadam@gmail.com>
2023-10-17 23:49:56 +0700

    updateInsertUser

--- main.go
+++ main.go
@@ -40,24 +40,24 @@ func GCFReturnStruct(DataStuct any) string {
 	return string(jsondata)
 }
 
-func InsertUser(MONGOCONNSTRINGENV, dbname, collectionname string, userdata User) string {
-	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
-	hash, _ := HashPassword(userdata.Password)
-	userdata.Password = hash
-	atdb.InsertOneDoc(mconn, collectionname, userdata)
-	return "Ini username : " + userdata.Username + " ini password : " + userdata.Password
-}
-
-// func InsertUser(r *http.Request) string {
-// 	var Response Credential
-// 	var userdata User
-// 		err := json.NewDecoder(r.Body).Decode(&userdata)
-// 		if err != nil { 
-// 			Response.Message = "error parsing application/json: " + err.Error() 
-// 			return GCFReturnStruct(Response) 
-// 		}
+// func InsertUser(MONGOCONNSTRINGENV, dbname, collectionname string, userdata User) string {
+// 	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
 // 	hash, _ := HashPassword(userdata.Password)
 // 	userdata.Password = hash
-// 	atdb.InsertOneDoc(SetConnection("MONGOSTRING", "db_urse"), " user", userdata)
-// 	return "Ini username : " + userdata.Username + "ini password : " + userdata.Password
+// 	atdb.InsertOneDoc(mconn, collectionname, userdata)
+// 	return "Ini username : " + userdata.Username + " ini password : " + userdata.Password
 // }
+
+func InsertUser(r *http.Request) string {
+	var Response Credential
+	var userdata User
+		err := json.NewDecoder(r.Body).Decode(&userdata)
+		if err != nil { 
+			Response.Message = "error parsing application/json: " + err.Error() 
+			return GCFReturnStruct(Response) 
+		}
+	hash, _ := HashPassword(userdata.Password)
+	userdata.Password = hash
+	atdb.InsertOneDoc(SetConnection("MONGOSTRING", "db_urse"), " user", userdata)
+	return "Ini username : " + userdata.Username + "ini password : " + userdata.Password
+}
--- urse_test.go
+++ urse_test.go
@@ -53,11 +53,11 @@ func TestIsPasswordValid(t *testing.T) {
 	fmt.Println(anu)
 }
 
-func TestInsertUser(t *testing.T){
-	var userdata User
-	userdata.Username = "dito"
-	userdata.Password = "secretoo"
+// func TestInsertUser(t *testing.T){
+// 	var userdata User
+// 	userdata.Username = "dito"
+// 	userdata.Password = "secretoo"
 
-	nama := InsertUser("MONGOSTRING", "urse", "user", userdata)
-	fmt.Println(nama)
-}
+// 	nama := InsertUser("MONGOSTRING", "urse", "user", userdata)
+// 	fmt.Println(nama)
+// }
