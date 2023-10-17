0f86a193ebb6549c8b6b3e3c91d638b2c9a5eea1
erditona <erditonaushaadam@gmail.com>
2023-10-17 14:35:08 +0700

    Push Be

---
+++ urse_test.go
@@ -0,0 +1,65 @@
+package beurse
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestGeneratePasswordHash(t *testing.T) {
+	password := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println("Ini Private", privateKey)
+	fmt.Println("Ini Public", publicKey)
+	hasil, err := watoken.Encode("petped", privateKey)
+	fmt.Println("Ini Hasil", hasil, err)
+}
+
+func TestHashFunction(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "urse")
+	var userdata User
+	userdata.Username = "dito"
+	userdata.Password = "secret"
+
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mconn, "user", filter)
+	fmt.Println("Mongo User Result: ", res)
+	hash, _ := HashPassword(userdata.Password)
+	fmt.Println("Hash Password : ", hash)
+	match := CheckPasswordHash(userdata.Password, res.Password)
+	fmt.Println("Match:   ", match)
+
+}
+
+func TestIsPasswordValid(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "urse")
+	var userdata User
+	userdata.Username = "dito"
+	userdata.Password = "secrett"
+
+	anu := IsPasswordValid(mconn, "user", userdata)
+	fmt.Println(anu)
+}
+
+func TestInsertUser(t *testing.T){
+	mconn := SetConnection("MONGOSTRING", "urse")
+	var userdata User
+	userdata.Username = "dito"
+	userdata.Password = "secret"
+	userdata.Role = "admin"
+
+	nama := InsertUser(mconn, "user", userdata)
+	fmt.Println(nama)
+}
